a83d9e8de04a70ad5b57d1e278376d8caa2b117b
hani88 <hani.abdulwahab@hotmail.com>
2020-10-22 22:39:06 +0200

    set style home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {FlightsService} from "../services/flights.service";
-
+import {Flight} from "../models/flights.model";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  flights : Flight[];
   constructor(private flightsService: FlightsService) { }
 
   ngOnInit(): void {
-    this.flightsService.getFlights();
+    this.flights = this.flightsService.getFlights();
   }
 
 }
--- src/app/services/flights.service.ts
+++ src/app/services/flights.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Flight} from "../models/flights.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
+  flights : Flight [ ] = [
+    { origin : 'Miami' ,
+      destination : 'Chicago',
+      flightNumber : 345 ,
+      depart : new Date('2020-02-25T23:18:21.932Z'),
+      arrive : new Date('2020-02-25T23:21:21.932Z'),
+      nonstop : true
+    },{
+    origin : " New York " ,
+      destination : 'Los Angeles',
+      flightNumber: 432,
+      depart :  new Date('2020-05-25T23:18:00.9327'),
+      arrive :  new Date('2020-05-25T23:23:21.932Z'),
+      nonstop : false
+  }] ;
 
   constructor() { }
 
   getFlights() {
-
+    return this.flights;
   }
 }
